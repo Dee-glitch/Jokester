6fe3fd647ce9785eb693dcbd5280cd1efbe9c9b7
Deedee N <you@example.com>
2021-04-06 11:51:21 +0200

    Using the web-speech-api

--- main.js
+++ main.js
@@ -1,49 +1,41 @@
 const button = document.querySelector('button');
-const audioElement = document.getElementById('audio');
 
 // Disable/enable button
 function toggleBtn() {
   button.disabled = !button.disabled;
 }
 
-// Passing joke to VoiceRSS API
+// Passing joke to API
 function tellMe(joke) {
-  VoiceRSS.speech({
-    key: "f004756715f64f679431c5138b61e47c",
-    src: joke,
-    hl: "en-us",
-    r: 0,
-    c: "mp3",
-    f: "44khz_16bit_stereo",
-    ssml: false,
-  });
+  const speech = new SpeechSynthesisUtterance();
+  speech.lang = 'en-US';
+  speech.text = joke;
+  speechSynthesis.speak(speech);
+  speech.addEventListener('end', toggleBtn)
 }
 
-  // Get jokes from joke API
-  async function getJokes() {
-    let joke = '';
-    const apiUrl =
-      "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
-
-    try {
-      const response = await fetch(apiUrl);
-      const data = await response.json();
-      if (data.setup) {
-          joke = `${data.setup} ... ${data.delivery}`;
-      } else {
-          joke = data.joke;
-      }
-      
-      //text-to-speech
-      tellMe(joke);
-      
-      // Disable button
-      toggleBtn();
-    } catch (error) {
-        document.querySelector('.container').textContent = error.message;
-    }
-  }
+// Get jokes from joke API
+async function getJokes() {
+  let joke = '';
   
-  button.addEventListener('click', getJokes);
-  audioElement.addEventListener('ended', toggleBtn);
+  try {
+    const apiUrl =
+      "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    if (data.setup) {
+        joke = `${data.setup} ... ${data.delivery}`;
+    } else {
+        joke = data.joke;
+    }
+    
+    tellMe(joke);
+    toggleBtn();
+
+  } catch (error) {
+      document.body.textContent = error.message;
+  }
+}
+
+button.addEventListener('click', getJokes);
 
