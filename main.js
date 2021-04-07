3e6a114db77c64c476c36bc8ffff1588d7691181
Deedee N <you@example.com>
2021-04-07 11:46:12 +0200

    add loader and better error handling

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const buttons = document.querySelectorAll('.btn');
+const container = document.querySelector('.container');
+const loader = document.getElementById("loader");
 const prog = document.getElementById('progBtn');
 const misc = document.getElementById("miscBtn");
 const spooky = document.getElementById("spookyBtn");
@@ -8,10 +10,9 @@ function toggleBtn() {
   buttons.forEach((button) => {
     button.disabled = !button.disabled;
   })
-  
 }
 
-// Passing joke to API
+// Passing joke to text-to-speech API
 function tellMe(joke) {
   const speech = new SpeechSynthesisUtterance();
   speech.lang = 'en-US';
@@ -25,24 +26,29 @@ async function getJokes(jokeCategory) {
   let joke = '';
   
   try {
+    loader.style.display = "block";
     const apiUrl =
       `https://v2.jokeapi.dev/joke/${jokeCategory}?blacklistFlags=nsfw,religious,political,racist,sexist,explicit`;
     const response = await fetch(apiUrl);
     
-    if(response.ok) {
+    if(response.status >= 200 && response.status < 400) {
+      loader.style.display = 'none';
       const data = await response.json();
       if (data.setup) {
         joke = `${data.setup} ... ${data.delivery}`;
       } else {
           joke = data.joke;
       }
-      
       tellMe(joke);
       toggleBtn();
+
+    } else {
+      throw "HTTP Error";
     }
 
   } catch (error) {
-      document.body.textContent = error.message;
+    container.classList.add('error');
+    container.textContent = error.message;
   }
 }
 
